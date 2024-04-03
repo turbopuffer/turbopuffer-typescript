3e01d7da214ce0883e09d57eb1d7a0bbddcb78e4
etcd <etcd>
2024-04-03 07:15:21 +0000

    Alphabetize rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,10 +13,6 @@ const config = {
   parserOptions: { project: true },
   plugins: ["@typescript-eslint", "import"],
   rules: {
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
-    ],
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       { prefer: "type-imports", fixStyle: "separate-type-imports" },
@@ -24,6 +20,10 @@ const config = {
     "@typescript-eslint/no-misused-promises": [
       2,
       { checksVoidReturn: { attributes: false } },
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
     ],
     "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
   },
