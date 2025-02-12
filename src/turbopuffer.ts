9753e1a47bf17ee5e050be138d07fb2cb05b8fa0
Arash-11 <arashnawy@gmail.com>
2025-02-12 17:22:53 -0500

    add namespace prefix param

--- src/turbopuffer.ts
+++ src/turbopuffer.ts
@@ -64,9 +64,11 @@ export class Turbopuffer {
    */
   async namespaces({
     cursor,
+    prefix,
     page_size,
   }: {
     cursor?: string;
+    prefix?: string,
     page_size?: number;
   }): Promise<NamespacesListResult> {
     return (
@@ -75,6 +77,7 @@ export class Turbopuffer {
         path: "/v1/namespaces",
         query: {
           cursor,
+          prefix,
           page_size: page_size ? page_size.toString() : undefined,
         },
         retryable: true,
