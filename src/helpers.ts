fb880bb0ed7ae6c9580355b4cc344570142aee92
Arash-11 <arashnawy@gmail.com>
2025-02-20 10:10:47 -0500

    check for userAgent too

--- src/helpers.ts
+++ src/helpers.ts
@@ -21,15 +21,15 @@ function detectRuntime(): Runtime {
   if (typeof globalThis.Deno !== "undefined")
     return "deno";
 
-  const { navigator } = globalThis;
+  const userAgent = globalThis.navigator?.userAgent;
 
   // Try navigator.userAgent:
   // https://developers.cloudflare.com/workers/runtime-apis/web-standards/#navigatoruseragent.
   // Fallback: look for presence of non-standard globals specific to the cloudflare runtime:
   // https://community.cloudflare.com/t/how-to-detect-the-cloudflare-worker-runtime/293715/2.
   if (
-    navigator
-      ? navigator.userAgent === "Cloudflare-Workers"
+    userAgent
+      ? userAgent === "Cloudflare-Workers"
       // @ts-expect-error can be ignored
       : typeof WebSocketPair !== "undefined"
   )
@@ -39,8 +39,8 @@ function detectRuntime(): Runtime {
     return "browser";
 
   if (
-    navigator
-      ? navigator.userAgent.startsWith("Node.js")
+    userAgent
+      ? userAgent.startsWith("Node.js")
       : process.release?.name === "node"
   )
     return "node";
