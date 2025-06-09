cc4d8fee5105037ccfa7f42dc49d017dc2c3d7f7
Nikhil Benesch <nikhil.benesch@gmail.com>
2025-06-09 13:15:17 -0400

    Revert "fix: restore `File` import in uploads.test.ts"
    This reverts commit 9b08f9068e41c7f5b747e804b77910f7d4203c60.

--- tests/uploads.test.ts
+++ tests/uploads.test.ts
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import type { ResponseLike } from '@turbopuffer/turbopuffer/internal/to-file';
 import { toFile } from '@turbopuffer/turbopuffer/core/uploads';
-import { File } from 'node:buffer';
 
 class MyClass {
   name: string = 'foo';
